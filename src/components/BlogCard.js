c2dc75bb0db8b07da1f30f0c7c1780de72ca7905
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-10-21 23:00:51 +0200

    Style the blog card component accordingly

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -5,7 +5,9 @@ const BlogCard = () => {
   return (
     <div className='col-3'>
      <div className='blog-card'>
-        
+        <div className="card-image">
+            <img src="./images/blog-1.jpg"   className='img-fluid' alt="blog" />
+        </div>
      </div>
     </div>
   )
