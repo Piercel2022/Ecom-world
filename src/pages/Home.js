6b36ca24064ba33543a707f26cc7fcb8212bac35
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-10-22 22:07:04 +0200

    Add another section to the main page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom'
 import Marquee from "react-fast-marquee";
 import BlogCard from '../components/BlogCard';
 import ProductCard from '../components/ProductCard';
+import SpecialProduct from '../components/SpecialProduct';
 const Home = () => {
   return (
     <>
@@ -189,6 +190,42 @@ const Home = () => {
         </div>
       </div>
     </section>
+    
+    <section className="featured-wrapper py-5 home-wrapper-2">
+    <div className="container-xxl">
+      <div className="row">
+        <div className="col-12">
+          <h3 className="section-heading">
+            Featured Collections
+          </h3>
+        </div>
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+      </div>
+    </div>
+    </section>
+
+    <section className="special-wrapper py-5 home-wrapper-2">
+      <div className="container-xxl">
+        <div className="row">
+        <div className="col-12">
+          <h3 className="section-heading">
+            Special Products
+          </h3>
+        </div>
+        </div>
+        <div className="row">
+          <SpecialProduct />
+          <SpecialProduct />
+          <SpecialProduct />
+        </div>
+      </div>
+
+
+    </section>
+
     <section className="marquee-wrapper py-5">
      <div className="container-xxl">
       <div className="row">
@@ -224,21 +261,6 @@ const Home = () => {
         </div>
       </div>
      </div>
-    </section>
-    <section className="featured-wrapper py-5 home-wrapper-2">
-    <div className="container-xxl">
-      <div className="row">
-        <div className="col-12">
-          <h3 className="section-heading">
-            Featured Collections
-          </h3>
-        </div>
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-      </div>
-    </div>
     </section>
     <section className="blog-wrapper py-5 home-wrapper-2">
     <div className="container-xxl">
