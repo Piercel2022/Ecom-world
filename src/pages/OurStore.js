b7918421cac77556c3eca64969b6b434adb708e9
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-10-26 22:02:29 +0200

    Add the workflow

--- src/pages/OurStore.js
+++ src/pages/OurStore.js
@@ -16,12 +16,20 @@ const OurStore = () => {
         <div className='filter-card mb-3'>
           <h3 className="filter-title">
            Shop By Categories
-          </h3>
+           </h3>
+           <div>
+            <ul className='ps-0'>
+              <li>Watch</li>
+              <li>Tv</li>
+              <li>Camera</li>
+              <li>Laptop</li>
+            </ul>
+           </div>
         </div>
         <div className='filter-card mb-3'>
         <h3 className="filter-title">
         Filter By 
-            </h3>
+        </h3>
         </div>
         <div className='filter-card mb-3'>
         <h3 className="filter-title">
