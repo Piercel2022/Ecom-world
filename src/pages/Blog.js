dca85a59f323e81069ee72b230523e42c5d76ab3
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-10-26 14:37:42 +0200

    Add workflow

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,15 +1,16 @@
+
 import React from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 
-const About = () => {
+const Blog = () => {
   return (
     <>
-     <Header />
-      About
-      <Footer />
+    <Header />
+    Blog
+    <Footer />
     </>
   )
 }
 
-export default About
+export default Blog
--- src/pages/OurStore.js
+++ src/pages/OurStore.js
@@ -1,12 +1,17 @@
 
 import React from 'react'
 import BreadCrumb from '../components/BreadCrumb'
+import {Helmet} from "react-helmet";
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 const OurStore = () => {
   return (
     <>
     <Header />
+    <Helmet>
+                <meta charSet="utf-8" />
+                <title>Our Store</title>
+            </Helmet>
     <BreadCrumb title = "Our Store"/>
     <Footer />
     </>
