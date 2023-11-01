adb0d2b129c3bb4825d7812124ff2950e87f9228
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-11-01 14:46:49 +0100

    Add the workflow

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,6 +2,8 @@
 import React from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import Meta from '../components/Meta'
+import BreadCrumb from '../components/BreadCrumb'
 
 const Contact = () => {
   return (
