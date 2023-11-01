08fd9c624476918ec2fe9fad8630b979f4afbfd0
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-11-01 14:47:55 +0100

    Add the workflow

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import Meta from '../components/Meta'
+import BreadCrumb from '../components/BreadCrumb'
 
 const About = () => {
   return (
