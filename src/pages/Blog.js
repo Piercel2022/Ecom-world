7de8d50feb352964cd59d7fdc2df9a9327c5c78e
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-11-01 14:35:40 +0100

    Add the blog section of the app

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -2,10 +2,14 @@
 import React from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import Meta from '../components/Meta'
+import BreadCrumb from '../components/BreadCrumb'
 
 const Blog = () => {
   return (
     <>
+    <Meta title={"Blogs"} />
+    <BreadCrumb title = "Blogs" />
     <Header />
     Blog
     <Footer />
