7c64a7c617890096b7d494dddfa4411b700ea0b3
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-11-02 19:18:16 +0100

    Add the workflow

--- src/pages/Wishlist.js
+++ src/pages/Wishlist.js
@@ -12,7 +12,9 @@ const Wishlist = () => {
     <Header />
     <BreadCrumb title = "wishlist" />
       <div className="wishlist-wrapper home-wrapper-2 py-3">
-
+        <div className="container-xxl">
+            
+        </div>
       </div>
     <Footer />
     </>
