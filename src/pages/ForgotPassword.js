75f343bc72864c819d2b12269eef2621c0057211
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-11-02 22:14:25 +0100

    Add the workflow

--- src/pages/ForgotPassword.js
+++ src/pages/ForgotPassword.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import Meta from '../components/Meta'
+import BreadCrumb from '../components/BreadCrumb'
 
 const ForgotPassword = () => {
   return (
     <>
+    <Meta title={"login"} />
+    <Header />
+    <BreadCrumb title = "login" />
     
+    <Footer />
     </>
   )
 }
