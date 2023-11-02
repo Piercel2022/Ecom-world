f56b3d6e6f18f62664b7b5573c465bc91089d5cd
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-11-02 21:36:00 +0100

    Add the workflow

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import Meta from '../components/Meta'
@@ -11,7 +12,28 @@ const Login = () => {
     <Header />
     <BreadCrumb title = "login" />
     <div className="login-wrapper home-wrapper-2 py-3">
-
+      <div className="row">
+        <div className="col-12">
+          <div className="auth-card">
+            <h3 className='text-center'>Login</h3>
+            <form action="" className='d-flex flex-column gap-10'>
+              <div>
+                <input type="email" name="email" placeholder='Email' className="form-control" />
+              </div>
+              <div>
+                <input type="password" name="password" placeholder='Password' className="form-control" />
+              </div>
+              <div>
+                <Link to="/forgot-password">Forgot Password?</Link>
+              </div>
+              <div className='d-flex justify-content-center align-items-center gap-15'>
+                <button className='button border-0'>Login</button>
+                <Link className='button signup'>Signup</Link>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
     <Footer />
     </>
