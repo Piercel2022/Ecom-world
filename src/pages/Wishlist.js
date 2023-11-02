14331270a6265fd34cbf8793c1ebf2ec7b1a1bfc
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-11-02 19:35:16 +0100

    Add the workflow

--- src/pages/Wishlist.js
+++ src/pages/Wishlist.js
@@ -15,8 +15,13 @@ const Wishlist = () => {
         <div className="container-xxl">
             <div className="row">
                 <div className="col-3">
-                    <div className="wishlist-card">
-                        
+                    <div className="wishlist-card  position-relative">
+                    <img src="images/cross.svg" alt="cross" className="position-absolute cross" />
+                        <div className="wishlist-card-image">
+                         <img src="images/watch.jpg" className='img-fluid w-100' alt="" />
+                        </div>
+                        <h5 className='title'></h5>
+                        <h6 className='price'>$ 100</h6>
                     </div>
                 </div>
             </div>
