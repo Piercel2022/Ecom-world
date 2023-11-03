223f621857bd4f27cf691fc0b8f2700c156fd6a3
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-11-03 13:57:18 +0100

    Add the workflow

--- src/pages/PrivacyPolicy.js
+++ src/pages/PrivacyPolicy.js
@@ -10,6 +10,17 @@ const PrivacyPolicy = () => {
     <Meta title={"privacy policy"} />
     <Header />
     <BreadCrumb title = "Privacy Policy" />
+    <section className="policy-wrapper py-5 home-wrapper-2">
+      <div className="container-xxl">
+        <div className="row">
+          <div className="col-12">
+            <div className="policy">
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
     <Footer />
     </>
   )
