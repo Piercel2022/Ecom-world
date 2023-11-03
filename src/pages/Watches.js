2c314e21f07f3e61b1000cda5ad7a9b148f09b8c
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-11-03 22:02:55 +0100

    Add the workflow

---
+++ src/pages/Watches.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import Meta from '../components/Meta'
+import BreadCrumb from '../components/BreadCrumb'
+const Watches = () => {
+  return (
+    <>
+    <Meta title={"watches"} />
+    <Header />
+    <BreadCrumb title = "Watches" />
+    
+    <Footer />
+    </>
+  )
+}
+
+export default Watches
