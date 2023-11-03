1682378e7cfcea86d2b379271fe5d47ea801e170
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-11-03 15:56:41 +0100

    Add the workflow

--- src/pages/SingleProduct.js
+++ src/pages/SingleProduct.js
@@ -27,9 +27,13 @@ const SingleProduct = () => {
            <div className="container-xxl">
             <div className="row">
               <div className="col-12">
-                <p className="bg-white p-3">
-
-                </p>
+                <h4 className=''>Description</h4>
+                <div className="bg-white p-3">
+                   <p>text ever since the 1500s, when an unknown printer took a 
+                    galley of type and scrambled it to make a type specimen book. 
+                    It has survived not only five centuries, but also the leap 
+                    into electronic typesetting, remaining essentially unchanged.</p>
+                </div>
               </div>
             </div>
            </div>
