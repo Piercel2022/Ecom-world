ace92c47238bcad17fdd65e79438e6fe5a3a7a2a
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-11-03 13:56:27 +0100

    Add the workflow

--- src/pages/ShippingPolicy.js
+++ src/pages/ShippingPolicy.js
@@ -10,6 +10,17 @@ const ShippingPolicy = () => {
     <Meta title={"shipping policy"} />
     <Header />
     <BreadCrumb title = "Shipping Policy" />
+    <section className="policy-wrapper py-5 home-wrapper-2">
+      <div className="container-xxl">
+        <div className="row">
+          <div className="col-12">
+            <div className="policy">
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
     <Footer />
     </>
   )
