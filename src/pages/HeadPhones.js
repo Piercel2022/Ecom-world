e466927bd2e6ed949a9a03fabffb225b8600f6cc
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-11-03 22:15:36 +0100

    Add the headphones component

---
+++ src/pages/HeadPhones.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import Meta from '../components/Meta'
+import BreadCrumb from '../components/BreadCrumb'
+const HeadPhones = () => {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default HeadPhones
