c0b2b0b5eeb2b5e218b7f8cc2bea1e6e1f9c7e37
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-11-03 11:43:09 +0100

    Add the font size to the title of the blog

--- src/pages/SingleBlog.js
+++ src/pages/SingleBlog.js
@@ -20,6 +20,11 @@ const SingleBlog = () => {
                     A beautiful Sunday Morning Renaissance
                 </h3>
                 <img src="/images/blog-1.jpg" alt="blog" />
+                <p>It is a long established fact that a reader will be distracted by 
+                  the readable content of a page when looking at its layout. 
+                  The point of using Lorem Ipsum is that it has a more-or-less 
+                  normal distribution of letters, as opposed to using 
+                  'Content here, content here', making it look like readable English.</p>
             </div>
           </div>
         </div>
