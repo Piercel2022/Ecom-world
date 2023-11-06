ee7482760c9d878677b366e6d2400e524bd78f04
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-11-06 01:04:51 +0100

    Add the workflow

--- src/components/SingleProduct.js
+++ src/components/SingleProduct.js
@@ -81,6 +81,9 @@ const SingleProduct = () => {
                     <div className='d-flex align-items-center gap-10 my-2'>
                     <h3 className='product-heading'> Avalaibility: </h3><p className='product-data'>In Stock</p>
                     </div>
+                    <div className='d-flex flex-column gap-10 my-2'>
+                    <h3 className='product-heading'> Tags: </h3><p className='product-data'>Gfd</p>
+                    </div>
                   </div>
                   
                 </div>
