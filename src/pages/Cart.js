56172c2f72500b3b7e9335eaac6c735de5dbc498
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-11-06 17:52:29 +0100

    Add the workflow

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,15 +1,18 @@
 
 import React from 'react'
-import Header from './Header'
-import Footer from './Footer'
-import Meta from './Meta'
-import BreadCrumb from './BreadCrumb'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import Meta from '../components/Meta'
+import BreadCrumb from '../components/BreadCrumb'
 const Cart = () => {
   return (
     <>
     <Meta title={"cart"} />
     <Header />
-    <BreadCrumb title = "Cart" />
+    <BreadCrumb title = "Your Shopping Cart" />
+    <section className="cart-wrapper">
+      
+    </section>
       
     <Footer />
     </>
