1fcd9dc6f8c6fea589c96b77727636bdf723adf9
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-11-07 14:10:18 +0100

    Add the caroussel to the home component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -19,8 +19,25 @@ const Home = () => {
         <div className="row">
           <div className="col-6">
            <div className="main-banner position-relative">
-            <img src='./images/main-banner.jpg'
-            className='img-fluid rounded' alt="main-banner" />
+           <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="./images/main-banner.jpg" class="d-block w-100" alt="banner"/>
+    </div>
+    <div class="carousel-item">
+      <img src="./images/main-banner1.jpg" class="d-block w-100" alt="banner1"/>
+    </div>
+    
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
             <div className="main-banner-content position-absolute">
               <h4>SUPERCHARGED FOR PROS.</h4>
               <h5>iPad S13+ Pro.</h5>
