8aae76423593c908100151d5f869120b1dfb9336
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-11-09 23:20:40 +0100

    Remove the container-xxl and replace the section with the container component

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -6,6 +6,7 @@ import {BsFillTelephoneFill} from 'react-icons/bs'
 import Footer from '../components/Footer'
 import Meta from '../components/Meta'
 import BreadCrumb from '../components/BreadCrumb'
+import Container from './Container'
 
 const Contact = () => {
   return (
@@ -13,8 +14,7 @@ const Contact = () => {
     <Meta title={"Contact"} />
     <Header />
     <BreadCrumb title = "Contact Us" />
-    <div className="contact-wrapper home-wrapper2 py-5">
-     <div className="container-xxl">
+    <Container class1="contact-wrapper home-wrapper2 py-5">
       <div className="row">
         <div className="col-12">
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10556.512067513686!2d7.706893372301644!3d48.588247640745735!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4796b7daff8249a5%3A0x60708f93d1f5c2e9!2sRue%20Marguerite%20Yourcenar%2C%2067200%20Strasbourg!5e0!3m2!1sfr!2sfr!4v1698852649022!5m2!1sfr!2sfr"
@@ -76,8 +76,7 @@ const Contact = () => {
 
         </div>
       </div>
-     </div>
-    </div>
+    </Container>
     <Footer />
     </>
   )
