2717662e4e4b52a2e7ad55c1474fba6ad70d9bcb
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-11-10 01:08:12 +0100

    Add the workflow

--- src/pages/Payment.js
+++ src/pages/Payment.js
@@ -4,6 +4,7 @@ import Footer from '../components/Footer'
 import Meta from '../components/Meta'
 import BreadCrumb from '../components/BreadCrumb'
 import { Link } from 'react-router-dom'
+import Container from './Container'
 
 const Payment = () => {
   return (
@@ -11,8 +12,7 @@ const Payment = () => {
     <Meta title={"payment"} />
     <Header />
     <BreadCrumb title = "Your Payment" />
-    <div className="payment-wrapper home-wrapper-2 py-5">
-      <div className="container-xxl">
+    <Container class1="payment-wrapper home-wrapper-2 py-5">
         <div className="row">
         <nav style={{'--bs-breadcrumb-divider': '>'}} aria-label="breadcrumb">
              <ol className="breadcrumb">
@@ -35,9 +35,7 @@ const Payment = () => {
               </nav>
         
         </div>
-      </div>
-
-    </div>
+      </Container>
 
     <Footer />
     </>
