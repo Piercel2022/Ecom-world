9e44e8d6ef8991e9381b892175d96a011848661f
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-11-10 10:49:58 +0100

    Add the home-wrapper-2 to the laptop component

--- src/pages/Laptops.js
+++ src/pages/Laptops.js
@@ -3,6 +3,7 @@ import Header from '../components/Header'
 import Footer from '../components/Footer'
 import Meta from '../components/Meta'
 import BreadCrumb from '../components/BreadCrumb'
+import Container from './Container'
 
 const Laptops = () => {
   return (
@@ -10,6 +11,9 @@ const Laptops = () => {
     <Meta title={"laptops"} />
     <Header />
     <BreadCrumb title = "Laptops" />
+    <Container className="laptop-wrapper home-wrapper-2 py-5">
+
+    </Container>
     <Footer />
         
     </>
